8114d254d5734856c4d92bc28618ca57d2dee2bd
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-05-01 23:59:04 +0530

    feat: 2026.05.01

---
+++ 2218C.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <deque>
+#include <queue>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+	ll n;
+	cin >> n;
+
+	ll k = 3 * n;
+	for (int i = 1; i <= n; i++) {
+		cout << i << " " << k << " " << k - 1 << " ";
+		k -= 2;
+	}
+	cout << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int testcases = 1;
+	cin >> testcases;
+
+	for (int tc = 1; tc <= testcases; tc++) {
+		solve();
+	}
+	return 0;
+}
+
